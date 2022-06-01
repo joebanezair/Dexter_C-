3e77dbf5fc6a7a5c83bd11c78a323e41e853137b
Joebanezair <49142653+joebanezair@users.noreply.github.com>
2022-06-01 17:24:32 +0800

    Update while_limit.cs

--- while_limit.cs
+++ while_limit.cs
@@ -1,122 +1,71 @@
 using System;
 
-namespace loop
+namespace ConsoleApp1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Random random = new Random();
-            int num = random.Next(1, 10);
-            int guess = 0;
-            int limit = 1;
-
-            Console.WriteLine("Guess a number between 1-100\nyou only got 3 attempts: ");
+            User use = new User("Joebanezair","Joe");
+            Administrator add = new Administrator("Joebanezair","0002","0002");
             
-           
-            while (limit <= 3){
-                guess = Convert.ToInt32(Console.Read());
-                if (num < guess) {
-                    Console.WriteLine(limit+". try again lower than "+ guess); Console.ReadLine();               
-                }
-                if (num > guess){
-                    Console.WriteLine(limit + ". try again higher than " + guess); Console.ReadLine();
-                }
-                if (num == guess){
-                    Console.WriteLine(limit + ". good job you finally made it in "+limit
-                    +" attempts and you guess it right : " + guess); 
-                    Console.ReadLine(); break;
-                }
-                guess = 1;
-                if (num == guess)
-                {
-                    Console.WriteLine(limit + ". good job you finally made it in " + limit 
-                    + " attempts and you guess it right : " + guess);
-                    Console.ReadLine(); break;
-                }
-                limit++;
-            }
-            Console.WriteLine("\nOpx you suceeded 3 attempts");
+            Console.WriteLine("Enter ID : "); string ID = Convert.ToString(Console.ReadLine());
+            add.verifyLogin(ID, "0002");
+
+            Console.WriteLine("Enter new Password : "); string pass = Convert.ToString(Console.ReadLine());
+            add.updataPassword(pass);
+
+            Console.WriteLine("Enter new updated password : "); string updated = Convert.ToString(Console.ReadLine());
+            use.updatePassword(updated);
+
+            Console.WriteLine("Enter updated Name : "); string updatedName = Convert.ToString(Console.ReadLine());
+            add.updateAdminName(updatedName);
 
 
         }
     }
-}
-
-
-
-
-
-
-
-
-
-using System;
-
-namespace UserNamespace
-{
-   
-
-    public class User  
+    class User
     {
-        private string user_id;
-        protected string user_passaword;
-
-        static void Main(string[] args)
-        {
-            User use;
-            string pass = "4321", id = "1234";
-            User.Equals(pass, id);
-            Console.WriteLine();
-        }
-
+        protected string user_id; protected string user_password;
         public User(string id, string pass) {
             this.user_id = id;
-            this.user_passaword = pass;
+            this.user_password = pass;
         }
-        public void verifyLogin (string id, string pass) {
-           this.user_id = id = "1234";
-           this.user_passaword = pass = "4321";
+        public void verifyLogin(string id, string pass) {
+            bool confirm = string.Equals(this.user_password, this.user_id);
 
-           bool compare = string.Equals(id, "1234");  
-           bool compare2 = string.Equals(pass, "4321");
-           Console.WriteLine(compare);
-           Console.WriteLine(compare2);
+            if (confirm.Equals(true))
+            {
+                Console.WriteLine("registered user : " + confirm);
+            }
+            else {
+                Console.WriteLine("registered user : " + confirm);
+            }
+           
         }
-        public void updatePassword
-        (string newPassword) {
-           this.user_passaword = newPassword;
+        public void updatePassword(string newPassword) {
+            this.user_password = newPassword;
+            Console.WriteLine("This is your new password : " +this.user_password);
         }
-        
     }
 
-    public class Administrator : User 
-    {
-        private string admin_name;
-        public string pass, id;
-        public Administrator
-        (string name, string id, string pass) : base(pass, id)  
-        {
-            this.admin_name = name = "Joebanezair";
-            this.id = id; 
-            this.pass = pass;
-        }
-        public void updatePassword
-        (string newPassword)
-        {
-            this.user_passaword = newPassword;
-        }
-        public void updateAdminName(string name)
-        {
-            this.admin_name = name;
-        }
+    class Administrator : User {
 
+        private string admin_name;
+        public Administrator(string name, string id, string pass) : base(id, pass) {
+            this.user_password = pass;
+            this.user_id = id;
+            this.admin_name = name;
+
+            Console.WriteLine("The data of the data of the user  \nUsername : "+this.admin_name+"\nUserID : "+ this.user_id+ "\nPassword : " + this.user_password + "\n\n");
+        }
+        public void updataPassword(string newPassword) {
+            this.user_password = newPassword;
+            Console.WriteLine("Updated password : " + this.user_password);
+        }
+        public void updateAdminName(string name) {
+            this.admin_name = name;
+            Console.WriteLine("Updated Admin Name : " + this.admin_name);
+        }
     }
 }
-
-     
-    
-
-
- 
-
